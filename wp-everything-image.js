b84125bf8516fae8779aab8faa0ee97bf5ca739e
Morgan Leek <morganleek@Morgans-iMac.local>
2019-03-20 17:08:56 +0800

    JS on load added

---
+++ wp-everything-image.js
@@ -0,0 +1,13 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+  // Lazyload for unsupported background images
+  var callback_reveal = function(element) {
+    if(jQuery(element).hasClass('scm-bg')) {
+      jQuery(element).addClass('loaded');
+    }
+  };
+
+  var lazyLoadInstance = new LazyLoad({
+    elements_selector: ".lazy",
+    callback_reveal: callback_reveal // For background images
+  });
+});
