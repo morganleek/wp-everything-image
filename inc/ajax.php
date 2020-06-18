016dc3ab617307ba6ba9cb0e6cf7e9c0062af9ba
Morgan Leek <morganleek@gmail.com>
2020-06-18 11:25:20 +0800

    Readme update

--- inc/ajax.php
+++ inc/ajax.php
@@ -1,5 +1,14 @@
 <?php
-	// Ajax 
+	// // How to use
+	// wp.ajax.send('wei_image', {
+	//   data: {
+	//     "action":"wei_image","image_id":5,"sizes":{"768":[768,300,true],"1":[350,200,true]}
+	//   },
+	//   error: function() {},
+	//   success: function(r) { console.log(r); },
+	//   type: 'POST'
+	// });
+	//
 	if( !function_exists( 'wei_image_ajax' ) ) {
 		function wei_image_ajax() {
 			// This needs to be sanitzed
