5327d39a9237d7df62e4db907abae2a06f2ba37c
morganleek <morganleek@gmail.com>
2021-08-31 08:33:30 +0000

    versioning

--- wp-everything-image.php
+++ wp-everything-image.php
@@ -3,7 +3,7 @@
 	Plugin Name: Everything Image
 	Plugin URI: http://morganleek.me/wordpress-2/everything-image
 	Description: Generate sized, lazy loaded, responsive HTML images and CSS background divs
-	Version: 1.3.1
+	Version: 1.3.2
 	Author: Morgan Leek
 	Author URI: https://morganleek.me/
 	Text Domain: everything-image
